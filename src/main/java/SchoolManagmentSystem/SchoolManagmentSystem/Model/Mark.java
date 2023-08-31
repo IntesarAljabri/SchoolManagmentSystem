ebdfc9900e734f6b5ba65cc3a8b879c7b0df7b09
Maan94919411* <intesaraljabri99@gmail.com>
2023-08-31 09:30:40 +0400

    Markclass

--- src/main/java/SchoolManagmentSystem/SchoolManagmentSystem/Model/Mark.java
+++ src/main/java/SchoolManagmentSystem/SchoolManagmentSystem/Model/Mark.java
@@ -1,4 +1,17 @@
 package SchoolManagmentSystem.SchoolManagmentSystem.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Data
+@Table(name = "mark")
 public class Mark {
+
+    String grade;
 }
