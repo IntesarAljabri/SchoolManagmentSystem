efff5cb6cac7fd61ece5b95ff46a97b07ef3f1ef
Maan94919411* <intesaraljabri99@gmail.com>
2023-08-31 09:33:06 +0400

    Courseclass

--- src/main/java/SchoolManagmentSystem/SchoolManagmentSystem/Model/Course.java
+++ src/main/java/SchoolManagmentSystem/SchoolManagmentSystem/Model/Course.java
@@ -1,4 +1,20 @@
 package SchoolManagmentSystem.SchoolManagmentSystem.Model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Data
+@Table(name = "course")
 public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer courseId;
+    String courseName;
+
 }
