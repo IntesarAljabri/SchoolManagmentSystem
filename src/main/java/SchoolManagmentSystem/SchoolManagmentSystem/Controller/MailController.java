16b51b6e37421e5ced3ac3a72c3286c1622ea1c3
Maan94919411* <intesaraljabri99@gmail.com>
2023-09-06 14:29:32 +0400

    Make the report and jrxml and add mailSendr

--- src/main/java/SchoolManagmentSystem/SchoolManagmentSystem/Controller/MailController.java
+++ src/main/java/SchoolManagmentSystem/SchoolManagmentSystem/Controller/MailController.java
@@ -3,6 +3,7 @@ package SchoolManagmentSystem.SchoolManagmentSystem.Controller;
 import SchoolManagmentSystem.SchoolManagmentSystem.Model.Email;
 import SchoolManagmentSystem.SchoolManagmentSystem.RepostService.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,9 @@ public class MailController {
         return status;
     }
 
-    @PostMapping("/attachement")
+
+
+    @Scheduled(cron = "/30 * * * * *")
     public String sendMailWithAttachment(@RequestBody Email email) {
         String status = emailService.sendMailWithAttachment(email);
         return status;
--- src/main/java/SchoolManagmentSystem/SchoolManagmentSystem/SchoolManagementSystemApplication.java
+++ src/main/java/SchoolManagmentSystem/SchoolManagmentSystem/SchoolManagementSystemApplication.java
@@ -2,8 +2,10 @@ package SchoolManagmentSystem.SchoolManagmentSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SchoolManagementSystemApplication{
 
 	public static void main(String[] args) {
